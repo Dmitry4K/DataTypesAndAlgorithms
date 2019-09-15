d166cae1693e5e7e40bc2d0360a3243f8dab3031
Dmitry <54765043+Dmitry4K@users.noreply.github.com>
2019-09-15 15:55:16 +0300

    Counting sort code
    Realization of counting sort with linear complexity O(n+k) were algorithm where K is the maximum number.
    Uses only with integers from 0 to K. Recommended to use under the condition n<<k

---
+++ Algorithms/Sorting/Counting Sort/counting_sort.h
@@ -0,0 +1,17 @@
+#pragma once
+void counting_sort(int* a, int size, int max_el) {
+	int* c = new int[max_el+1];
+	for (int i = 0; i <= max_el; i++)
+		c[i] = 0;
+	for (int i = 0; i < size; i++)
+		c[a[i]]++;
+	for (int i = 1; i <= max_el; i++)
+		c[i] += c[i - 1];
+	int* b = new int[size];
+	for (int i = size - 1; i >= 0; i--) {
+		c[a[i]]--;
+		b[c[a[i]]] = a[i];
+	}
+	for (int i = 0; i < size; i++)
+		a[i] = b[i];
+}
