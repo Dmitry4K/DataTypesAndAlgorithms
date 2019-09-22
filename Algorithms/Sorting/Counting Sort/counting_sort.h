94009ba9cacb7a305d476dbacb9bd9a79983b687
Dmitry <54765043+Dmitry4K@users.noreply.github.com>
2019-09-22 12:15:30 +0300

    Update counting_sort.h

--- Algorithms/Sorting/Counting Sort/counting_sort.h
+++ Algorithms/Sorting/Counting Sort/counting_sort.h
@@ -7,7 +7,7 @@ void counting_sort(unsigned int* a, int size, int max_el) {
 		c[a[i]]++;
 	for (int i = 1; i <= max_el; i++)
 		c[i] += c[i - 1];
-	unsigned int* b = new int[size];
+	unsigned int* b = new unsigned int[size];
 	for (int i = size - 1; i >= 0; i--) {
 		c[a[i]]--;
 		b[c[a[i]]] = a[i];
