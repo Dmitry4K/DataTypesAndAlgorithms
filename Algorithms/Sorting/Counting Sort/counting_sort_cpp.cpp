c55031dc2e29b341fc0d2d81ee94cade1cc29b7c
Dmitry Korostelev <54765043+Dmitry4K@users.noreply.github.com>
2020-04-05 19:57:49 +0300

    Create counting_sort_cpp.cpp

---
+++ Algorithms/Sorting/Counting Sort/counting_sort_cpp.cpp
@@ -0,0 +1,24 @@
+#include<vector>
+template<class T>
+void countingSort(std::vector<T>&a) {
+	T maxEl = a[0];
+	for (int i = 1; i < a.size(); ++i) {
+		if (maxEl < a[i]) {
+			maxEl = a[i];
+		}
+	}
+	std::vector<T> c(maxEl + 1);
+	for (int i = 0; i <= maxEl; ++i)
+		c[i] = 0;
+	for (int i = 0; i < a.size(); ++i)
+		c[a[i]]++;
+	for (int i = 1; i <= maxEl; ++i)
+		c[i] += c[i - 1];
+	std::vector<T> b(a.size());
+	for (int i = size - 1; i >= 0; i--) {
+		c[a[i]]--;
+		b[c[a[i]]] = a[i];
+	}
+	for (int i = 0; i < size; i++)
+		a[i] = b[i];
+}
