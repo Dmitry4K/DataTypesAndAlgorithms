99c0ed44398492c5c4bafe34c36714043c56ca87
Dmitry Korostelev <54765043+Dmitry4K@users.noreply.github.com>
2020-05-05 21:27:00 +0300

    Update counting_sort_cpp.cpp

--- Algorithms/Sorting/Counting Sort/counting_sort_cpp.cpp
+++ Algorithms/Sorting/Counting Sort/counting_sort_cpp.cpp
@@ -1,23 +1,23 @@
 template<class T>//only indexed types : int , long int , unsigned int , etc
 void countingSort(std::vector<T>&a) {
 	T maxEl = a[0];
-	for (int i = 1; i < a.size(); ++i) {
+	for (size_t i = 1; i < a.size(); ++i) {
 		if (maxEl < a[i]) {
 			maxEl = a[i];
 		}
 	}
 	std::vector<T> c(maxEl + 1);
-	for (int i = 0; i <= maxEl; ++i)
+	for (size_t i = 0; i <= maxEl; ++i)
 		c[i] = 0;
-	for (int i = 0; i < a.size(); ++i)
+	for (size_t i = 0; i < a.size(); ++i)
 		c[a[i]]++;
-	for (int i = 1; i <= maxEl; ++i)
+	for (size_t i = 1; i <= maxEl; ++i)
 		c[i] += c[i - 1];
 	std::vector<T> b(a.size());
-	for (int i = a.size() - 1; i >= 0; i--) {
+	for (size_t i = a.size() - 1; i >= 0; i--) {
 		c[a[i]]--;
 		b[c[a[i]]] = a[i];
 	}
-	for (int i = 0; i < a.size(); i++)
+	for (size_t i = 0; i < a.size(); i++)
 		a[i] = b[i];
 }
