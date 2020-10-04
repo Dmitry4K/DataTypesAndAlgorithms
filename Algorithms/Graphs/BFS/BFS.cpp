99c78fc41eff9ae092a957f6a60884254d3718d7
Dmitry Korostelev <54765043+Dmitry4K@users.noreply.github.com>
2020-10-04 18:10:11 +0300

    Create BFS.cpp

---
+++ Algorithms/Graphs/BFS/BFS.cpp
@@ -0,0 +1,20 @@
+#include<vector>
+#include<queue>
+
+bfs(std::vector<std::vector<int>> g, int s){
+  queue<int> q;
+  q.push (s);
+  vector<bool> used (n);
+  used[s] = true;
+  while (!q.empty()) {
+  	int v = q.front();
+  	q.pop();
+  	for (size_t i=0; i<g[v].size(); ++i) {
+  		int to = g[v][i];
+  		if (!used[to]) {
+  			used[to] = true;
+  			q.push (to);
+  		}
+  	}
+  }
+}
