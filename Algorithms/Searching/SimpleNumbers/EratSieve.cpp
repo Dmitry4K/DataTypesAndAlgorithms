b4a45a83a52b54f94df51dc6918134e3400fe477
Dmitry Korostelev <54765043+Dmitry4K@users.noreply.github.com>
2020-10-04 18:04:34 +0300

    Create EratSieve.cpp

---
+++ Algorithms/Searching/SimpleNumbers/EratSieve.cpp
@@ -0,0 +1,14 @@
+#include<vector>
+
+void getSimpleNumbers(std::vector<int>& a, int n) {
+	std::vector<bool> numbers(n + 1);
+	numbers[0] = numbers[1] = true;
+	for (int i = 2; i <= n; ++i) {
+		if (!numbers[i]) {
+			a.push_back(i);
+			for (int j = i; j <= n; j += i) {
+				numbers[j] = true;
+			}
+		}
+	}
+}
