b179c1871a4a35e630574272b54dd50fa9ed3613
Dmitry Korostelev <54765043+Dmitry4K@users.noreply.github.com>
2020-10-05 12:49:58 +0300

    Create MaxAscendingSubsequence.cpp

---
+++ Algorithms/Dynamic/MaxAscendingSubsequence.cpp
@@ -0,0 +1,16 @@
+void MaxAscendingSubsequence(std::vector<int>& a, int n, std::vector<int>& d, std::vector<int>& p){
+  d.resize(n);
+  p.resize(n);
+	p[0] = -1;
+	for (int i = 0; i < n; ++i) {
+		d[i] = 1;
+		p[i] = -1;
+		for (int j = 0; j < i; ++j)
+			if (a[j] < a[i] ) {
+				if (1 + d[j] > d[i]) {
+					p[i] = j;
+					d[i] = 1 + d[j];
+				}
+			}
+	}
+}
