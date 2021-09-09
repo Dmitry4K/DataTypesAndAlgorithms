64a28376f2320058559926a5f87f7f81c30dba77
Dmitry Korostelev <54765043+Dmitry4K@users.noreply.github.com>
2021-09-09 17:09:31 +0300

    Update SimpleTimer.hpp

--- DataTypes/SimpleTimer/SimpleTimer.hpp
+++ DataTypes/SimpleTimer/SimpleTimer.hpp
@@ -1,19 +1,21 @@
 #pragma once
 #include<chrono>
+template<class T = std::chrono::nanoseconds>
 class Timer {
-	std::chrono::steady_clock::time_point start_point = std::chrono::high_resolution_clock::now();
-	std::chrono::steady_clock::time_point end_point = start_point;
-	bool is_stoped = true;
+    std::chrono::steady_clock::time_point start_point = std::chrono::high_resolution_clock::now();
+    std::chrono::steady_clock::time_point end_point = start_point;
+    bool is_stoped = true;
 public:
-	void Start(){
-    start_point = std::chrono::high_resolution_clock::now();
-    is_stoped = false;
-  }
-	void Stop(){
-    end_point = std::chrono::high_resolution_clock::now();
-    is_stoped = true;
-  }
-	long long Time(){
-    return is_stoped ? std::chrono::duration_cast<std::chrono::milliseconds>(end_point - start_point).count() : std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_point).count();
-  }
+    using time_type = T;
+    void Start() {
+        start_point = std::chrono::high_resolution_clock::now();
+        is_stoped = false;
+    }
+    void Stop() {
+        end_point = std::chrono::high_resolution_clock::now();
+        is_stoped = true;
+    }
+    long long Time() {
+        return is_stoped ? std::chrono::duration_cast<time_type>(end_point - start_point).count() : std::chrono::duration_cast<time_type>(std::chrono::high_resolution_clock::now() - start_point).count();
+    }
 };
