667b90326b7a4eefde74c419478f5f6987808687
Dmitry Korostelev <54765043+Dmitry4K@users.noreply.github.com>
2021-10-15 19:41:40 +0300

    Update SimpleTimer.hpp

--- DataTypes/SimpleTimer/SimpleTimer.hpp
+++ DataTypes/SimpleTimer/SimpleTimer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include<chrono>
 
+
 namespace chrono_timer {
     template<class T = std::chrono::nanoseconds>
     class timer {
@@ -9,22 +10,40 @@ namespace chrono_timer {
         bool is_stoped = true;
     public:
         using time_type = T;
-        void Start() {
+        void start() {
             start_point = std::chrono::high_resolution_clock::now();
             is_stoped = false;
         }
-        void Stop() {
+        void stop() {
             end_point = std::chrono::high_resolution_clock::now();
             is_stoped = true;
         }
-        long long Time() {
+        long long time() {
             return is_stoped ? std::chrono::duration_cast<time_type>(end_point - start_point).count() : std::chrono::duration_cast<time_type>(std::chrono::high_resolution_clock::now() - start_point).count();
         }
+        long long measure(void f(void)) {
+            this->start();
+            f();
+            return this->time();
+        }
     };
-    long long nanosecondsTime(void f(void)) {
+    long long nanoseconds_time(void f(void)) {
         timer<> timer;
-        timer.Start();
+        timer.start();
         f();
-        return timer.Time();
+        return timer.time();
     }
+
+    template<class T>
+    long long measure_time(void f(void)) {
+        timer<T> timer;
+        timer.start();
+        f();
+        return timer.time();
+    }
+
+    using timer_nns = timer <>;
+    using timer_mls = timer < std::chrono::milliseconds >;
+    using timer_mcs = timer < std::chrono::microseconds >;
+    using timer_sec = timer < std::chrono::seconds >;
 };
